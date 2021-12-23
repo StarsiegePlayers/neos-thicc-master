a18cd99cb014ce62accd3b6a47aa13d574c91c34
Neo-Desktop <thepureamrit@gmail.com>
2021-12-23 10:25:46 -0800

    ensure we sort the masters and games in the api by ping
    	modified:   httpd-impl.go
    	new file:   httpd-serverlistdata.go

--- httpd-impl.go
+++ httpd-impl.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/fs"
 	"net/http"
+	"sort"
 	"time"
 
 	"github.com/StarsiegePlayers/darkstar-query-go/v2/query"
@@ -14,13 +15,6 @@ type MasterQuery struct {
 	ServerCount int
 }
 
-type ServerListData struct {
-	RequestTime time.Time
-	Masters     []*MasterQuery
-	Games       []*query.PingInfoQuery
-	Errors      []string
-}
-
 func (s *HTTPDService) registerRoutes() {
 	s.router.SetFileSystem(fs.Sub(wwwFS, "www-build"))
 	s.router.AddRoute("/api/v1/admin/serversettings", http.MethodGet, s.middlewareAuth(s.routeGetAdminServerSettings))
@@ -28,6 +22,7 @@ func (s *HTTPDService) registerRoutes() {
 	s.router.AddRoute("/api/v1/admin/login", http.MethodPost, s.middlewareThrottle(s.routePostAdminLogin))
 	s.router.AddRoute("/api/v1/admin/login", http.MethodGet, s.middlewareThrottle(s.routeGetAdminLoginStatus))
 	s.router.AddRoute("/api/v1/multiplayer/servers", http.MethodGet, http.HandlerFunc(s.routeGetMultiplayerServers))
+	s.router.AddRoute("/yeet", http.MethodGet, http.HandlerFunc(s.routeGetYeeted))
 }
 
 func (s *HTTPDService) middlewareAuth(next http.HandlerFunc) http.Handler {
@@ -45,9 +40,10 @@ func (s *HTTPDService) middlewareThrottle(next http.HandlerFunc) http.Handler {
 }
 
 func (s *HTTPDService) routeGetMultiplayerServers(w http.ResponseWriter, r *http.Request) {
-	data, ok := s.cache["multiplayer"].(*ServerListData)
+	cacheData, ok := s.cache["MultiplayerServers"].(*ServerListData)
 
-	if !ok || data.RequestTime.Before(time.Now().Add(-60*time.Second)) {
+	// only update when stale
+	if !ok || cacheData.RequestTime.Before(time.Now().Add(-60*time.Second)) {
 		mstr := (*s.Services)["master"].(*MasterService)
 		mstr.Lock()
 		games := make([]*query.PingInfoQuery, 0)
@@ -72,19 +68,26 @@ func (s *HTTPDService) routeGetMultiplayerServers(w http.ResponseWriter, r *http
 		}
 		poll.Unlock()
 
-		data = &ServerListData{
+		// update the cache
+		cacheData = &ServerListData{
 			RequestTime: time.Now(),
 			Masters:     masters,
 			Games:       games,
 			Errors:      errors,
 		}
-		s.cache["multiplayer"] = data
+
+		sort.Sort(cacheData.Masters)
+		sort.Sort(cacheData.Games)
+
+		s.Lock()
+		s.cache["MultiplayerServers"] = cacheData
+		s.Unlock()
 	}
 
 	w.Header().Add("Content-Type", "application/json")
-	w.Header().Add("Last-Modified", data.RequestTime.Format(time.RFC1123))
+	w.Header().Add("Last-Modified", cacheData.RequestTime.Format(time.RFC1123))
 
-	jsonOut, err := json.Marshal(data)
+	jsonOut, err := json.Marshal(cacheData)
 	if err != nil {
 		s.LogAlert("error marshalling api server list %s", err)
 		return
@@ -98,6 +101,23 @@ func (s *HTTPDService) routeGetMultiplayerServers(w http.ResponseWriter, r *http
 }
 
 func (s *HTTPDService) routeGetAdminLoginStatus(w http.ResponseWriter, r *http.Request) {
+	jsonOut, err := json.Marshal(struct {
+		LoggedIn bool
+		User     string
+	}{
+		LoggedIn: false,
+		User:     "",
+	})
+	if err != nil {
+		s.LogAlert("error marshalling api server list %s", err)
+		return
+	}
+
+	_, err = w.Write(jsonOut)
+	if err != nil {
+		s.LogAlert("error writing api server list %s", err)
+		return
+	}
 
 }
 
@@ -111,4 +131,9 @@ func (s *HTTPDService) routePutAdminServerSettings(w http.ResponseWriter, r *htt
 
 func (s *HTTPDService) routeGetAdminServerSettings(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func (s *HTTPDService) routeGetYeeted(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Location", "https://youtu.be/pY725Ya74VU")
+	w.WriteHeader(http.StatusTemporaryRedirect)
 }
---
+++ httpd-serverlistdata.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"time"
+
+	"github.com/StarsiegePlayers/darkstar-query-go/v2/query"
+)
+
+type ServerListData struct {
+	RequestTime time.Time
+	Masters     MastersByPing
+	Games       PingInfoQueryByPing
+	Errors      []string
+}
+
+type MastersByPing []*MasterQuery
+
+func (m MastersByPing) Len() int           { return len(m) }
+func (m MastersByPing) Less(i, j int) bool { return m[i].Ping < m[j].Ping }
+func (m MastersByPing) Swap(i, j int)      { m[i], m[j] = m[j], m[i] }
+
+type PingInfoQueryByPing []*query.PingInfoQuery
+
+func (p PingInfoQueryByPing) Len() int           { return len(p) }
+func (p PingInfoQueryByPing) Less(i, j int) bool { return p[i].Ping < p[j].Ping }
+func (p PingInfoQueryByPing) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
